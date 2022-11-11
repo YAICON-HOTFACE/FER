28f8624b5d56d89cbb2861bb99ac06e0d470b492
Junyoung Park <79881119+junia3@users.noreply.github.com>
2022-11-11 17:12:39 +0900

    Update model_factory.py

--- FaceEmotionTrain/model_factory.py
+++ FaceEmotionTrain/model_factory.py
@@ -8,6 +8,8 @@ from models.mobilenetv2 import MobileNetV2
 from models.efficientnet import EfficientNetLite
 from models.mobilenetv3 import mobilenetv3
 from models.edgenext import create_edgenext_xx_small
+from facenet_pytorch import InceptionResnetV1
+from models.convnext import *
 # except:
 #     from resnet import *
 #     from edgenext import *
@@ -21,6 +23,9 @@ import time
 import os
 import numpy as np
 from timm.models import create_model
+from efficientnet_pytorch import EfficientNet
+import timm
+import pdb
 
 def model_build(model_name:str, num_classes:int, in_channel:int):
     """
@@ -83,6 +88,54 @@ def model_build(model_name:str, num_classes:int, in_channel:int):
     if model_name == 'mobilenetv3_0.75':
         model = mobilenetv3(num_classes=num_classes, width_mult=0.75)
 
+    if model_name == 'inceptionresnet':
+        model = InceptionResnetV1(pretrained='vggface2', classify=True)
+        model.logits = nn.Linear(512, num_classes, bias=True)
+
+    if model_name == 'b0':
+        model = EfficientNet.from_pretrained('efficientnet-b0')
+        model._fc = nn.Linear(1280, num_classes, bias=True)
+    
+    if model_name == 'b1':
+        model = EfficientNet.from_pretrained('efficientnet-b1')
+        model._fc = nn.Linear(1280, num_classes, bias=True)
+
+    if model_name == 'b2':
+        model = EfficientNet.from_pretrained('efficientnet-b2')
+        model._fc = nn.Linear(1408, num_classes, bias=True)
+
+    if model_name == 'b3':
+        model = EfficientNet.from_pretrained('efficientnet-b3')
+        model._fc = nn.Linear(1280, num_classes, bias=True)
+
+    if model_name == 'b4':
+        model = EfficientNet.from_pretrained('efficientnet-b4')
+        model._fc = nn.Linear(1792, num_classes, bias=True)
+    
+    if model_name == 'b5':
+        model = EfficientNet.from_pretrained('efficientnet-b5')
+        model._fc = nn.Linear(1280, num_classes, bias=True)
+
+    if model_name == 'b6':
+        model = EfficientNet.from_pretrained('efficientnet-b6')
+        model._fc = nn.Linear(1280, num_classes, bias=True)
+    
+    if model_name == 'b7':
+        model = EfficientNet.from_pretrained('efficientnet-b7')
+        model._fc = nn.Linear(1280, num_classes, bias=True)
+
+    if model_name == 'convnext_tiny':
+        model = convnext_tiny(pretrained=True)
+        model.head = nn.Linear(768, num_classes, bias=True)
+        
+    if model_name == 'convnext_small':
+        model = convnext_small(pretrained=True)
+        model.head = nn.Linear(768, num_classes, bias=True)
+        
+    if model_name == 'convnext_base':
+        model = convnext_base(pretrained=True)
+        model.head = nn.Linear(1024, num_classes, bias=True)
+        
     return model
 
 
